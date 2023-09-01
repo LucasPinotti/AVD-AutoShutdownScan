0bc7020463c7b271cfd32687e3526b58fa6b37d0
LucasPinotti <93174151+LucasPinotti@users.noreply.github.com>
2023-09-01 19:41:04 -0300

    Update RunBook.ps1

--- RunBook.ps1
+++ RunBook.ps1
@@ -8,6 +8,7 @@ catch {
     throw $_.Exception
 }
 
+#Prueba 1234
 ######## Variables ##########
 ## Update "HostPool" value with your host pool, and "HostPoolRG" with the value of the host pool resource group.
 ## See the next step if working with multiple host pools.
